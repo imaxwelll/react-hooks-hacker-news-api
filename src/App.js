2a132876451cea5c67faa12d48b2c28479f0680a
Maksym Bilashenko <mbilashenko@conceptsoft.io>
2021-02-25 19:27:29 +0200

    Add loader text.

--- src/App.js
+++ src/App.js
@@ -3,49 +3,58 @@ import axios from "axios";
 
 export default function App() {
 	const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('react hooks');
+	const [query, setQuery] = useState("react hooks");
+	const [loading, setLoading] = useState(false);
 	const searchInputRef = useRef();
 
 	useEffect(() => {
 		getResults();
 	}, []);
 
-  const getResults = async () => {
-    const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}`
-    );
-     setResults(response.data.hits);
-  }
+	const getResults = async () => {
+		setLoading(true);
+		const response = await axios.get(
+			`http://hn.algolia.com/api/v1/search?query=${query}`
+		);
+		setResults(response.data.hits);
+		setLoading(false);
+	};
 
-	const handleSearch = event => {
+	const handleSearch = (event) => {
 		event.preventDefault();
 		getResults();
-	}
+	};
 
-	const handleClearSearch = event => {
+	const handleClearSearch = (event) => {
 		setQuery("");
 		searchInputRef.current.focus();
-	}
+	};
 
-	return (	
+	return (
 		<>
 			<form onSubmit={handleSearch}>
-				<input 
+				<input
 					type="text"
-					onChange={event => setQuery(event.target.value)}
+					onChange={(event) => setQuery(event.target.value)}
 					value={query}
 					ref={searchInputRef}
 				/>
 				<button type="submit">Search</button>
-				<button type="button" onClick={handleClearSearch}>Clear</button>
+				<button type="button" onClick={handleClearSearch}>
+					Clear
+				</button>
 			</form>
-			<ul>
-				{results.map((result) => (
-					<li key={result.objectID}>
-						<a href={result.url}>{result.title}</a>
-					</li>
-				))}
-			</ul>
+			{loading ? (
+				<div>Loading...</div>
+			) : (
+				<ul>
+					{results.map((result) => (
+						<li key={result.objectID}>
+							<a href={result.url}>{result.title}</a>
+						</li>
+					))}
+				</ul>
+			)}
 		</>
 	);
 }
