db671b7a98453fbb70ae223009eeaf8e52c71a28
Maksym Bilashenko <mbilashenko@conceptsoft.io>
2021-02-27 20:02:16 +0200

    Add formating

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ export default function App() {
 				`http://hn.algolia.com/api/v1/search?query=${query}`
 			);
 			setResults(response.data.hits);
-		} catch (err){
-			setError(err)
+		} catch (err) {
+			setError(err);
 		}
 		setLoading(false);
 	};
@@ -37,12 +37,13 @@ export default function App() {
 
 	return (
 		<div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
-			<img src="https://icon.now.sh/react/c0c"
-			alt="react logo"
-			className="float-right h-12"/>
+			<img
+				src="https://icon.now.sh/react/c0c"
+				alt="react logo"
+				className="float-right h-12"
+			/>
 			<h1 className="text-grey-darkest font-thin">Hooks News</h1>
-			<form onSubmit={handleSearch}
-			className="mb-2">
+			<form onSubmit={handleSearch} className="mb-2">
 				<input
 					type="text"
 					onChange={(event) => setQuery(event.target.value)}
@@ -50,10 +51,14 @@ export default function App() {
 					ref={searchInputRef}
 					className="border p-1 rounded"
 				/>
-				<button type="submit"
-				className="bg-orange rounded m-1 p-1">Search</button>
-				<button type="button" onClick={handleClearSearch}
-				className="bg-teal text-white p-1 rounded">
+				<button type="submit" className="bg-orange rounded m-1 p-1">
+					Search
+				</button>
+				<button
+					type="button"
+					onClick={handleClearSearch}
+					className="bg-teal text-white p-1 rounded"
+				>
 					Clear
 				</button>
 			</form>
@@ -62,16 +67,17 @@ export default function App() {
 			) : (
 				<ul className="list-reset leading-normal">
 					{results.map((result) => (
-						<li key={result.objectID}
-						className="text-indigo-dark hover:text-indigo-darkest">
+						<li
+							key={result.objectID}
+							className="text-indigo-dark hover:text-indigo-darkest"
+						>
 							<a href={result.url}>{result.title}</a>
 						</li>
 					))}
 				</ul>
 			)}
 
-			{error && <div
-			className="text-red font-bold">{error.message}</div>}
+			{error && <div className="text-red font-bold">{error.message}</div>}
 		</div>
 	);
 }
