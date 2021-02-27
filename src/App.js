09e3263598394ecbed89806fcc18c7ba9feab256
Maksym Bilashenko <mbilashenko@conceptsoft.io>
2021-02-27 20:01:20 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -36,32 +36,42 @@ export default function App() {
 	};
 
 	return (
-		<>
-			<form onSubmit={handleSearch}>
+		<div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+			<img src="https://icon.now.sh/react/c0c"
+			alt="react logo"
+			className="float-right h-12"/>
+			<h1 className="text-grey-darkest font-thin">Hooks News</h1>
+			<form onSubmit={handleSearch}
+			className="mb-2">
 				<input
 					type="text"
 					onChange={(event) => setQuery(event.target.value)}
 					value={query}
 					ref={searchInputRef}
+					className="border p-1 rounded"
 				/>
-				<button type="submit">Search</button>
-				<button type="button" onClick={handleClearSearch}>
+				<button type="submit"
+				className="bg-orange rounded m-1 p-1">Search</button>
+				<button type="button" onClick={handleClearSearch}
+				className="bg-teal text-white p-1 rounded">
 					Clear
 				</button>
 			</form>
 			{loading ? (
-				<div>Loading...</div>
+				<div className="font-bold text-orange-dark">Loading...</div>
 			) : (
-				<ul>
+				<ul className="list-reset leading-normal">
 					{results.map((result) => (
-						<li key={result.objectID}>
+						<li key={result.objectID}
+						className="text-indigo-dark hover:text-indigo-darkest">
 							<a href={result.url}>{result.title}</a>
 						</li>
 					))}
 				</ul>
 			)}
 
-			{error && <div>{error.message}</div>}
-		</>
+			{error && <div
+			className="text-red font-bold">{error.message}</div>}
+		</div>
 	);
 }
